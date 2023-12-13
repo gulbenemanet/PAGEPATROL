f9dc73cb2daf8a35cfd11a8121c736b2510f41dc
Çağrı Yavuz <cagriiyavuzz@gmail.com>
2023-12-13 19:02:26 +0300

    Application initialized.
    Flutter kuruldu.

---
+++ front-end/pagepatrol_front/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return  const MaterialApp(
+      title: "PagePatrol",
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("pagepatrol"),
+      ),
+      body: const Text("İnitial App"),
+    );
+
+  }}

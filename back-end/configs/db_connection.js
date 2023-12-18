11da2e7c9e5ca5acc93ccd31ffadb466e4c3c069
gulbenemanet <gulbenemanet@gmail.com>
2023-12-19 00:54:27 +0300

    no message

--- back-end/configs/db_connection.js
+++ back-end/configs/db_connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://gulbenemanet:Rq8CIzUdjV479AjE@pagepatrol.r1sqrqf.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
